cca0e733aa0f2724a2b11f27911702a0748c9c9e
Samuel Michael <sam.michael26@gmail.com>
2024-05-21 20:10:21 +0700

    Project Skripsi

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HeadNav from '../Layouts/HeadNav.jsx';
+
+const Home = () => {
+    return (
+        <HeadNav>
+        <h1>Welcome to My App</h1>
+        <p>This is a simple static navbar with dropdowns using Bootstrap in React with Inertia.js.</p>
+        </HeadNav>
+    );
+};
+
+export default Home;
