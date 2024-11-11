84490b526d8520e74eb7b9d4ddb1e4c34c9c5328
Cheapfan <stevencokro32@gmail.com>
2024-11-11 22:01:14 +0700

    Add animal function

--- app/Http/Controllers/internal/animal/DataHewanController.php
+++ app/Http/Controllers/internal/animal/DataHewanController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class DataHewanController extends Controller
 {
-    public function index(){
+    public function viewListAnimal(){
 
         return view('internal.content.animal.animalDashboard', [
             'title' => 'Data Hewan',
@@ -15,4 +15,41 @@ class DataHewanController extends Controller
             'pageSubTitle' => 'Data Hewan Kucing dan Anjing',
         ]);
     }
+
+    public function viewDetailAnimal($id){
+
+        return view('internal.content.animal.animalDetail', [
+            'title' => 'Data Hewan',
+            'pageTitle' => 'Detail Data Hewan',
+            'pageSubTitle' => 'Detail Data Hewan Kucing dan Anjing',
+            'id' => $id,
+        ]);
+    }
+
+    public function createAnimal(){
+
+        return view('internal.content.animal.animalCreate', [
+            'title' => 'Data Hewan',
+            'pageTitle' => 'Tambah Data Hewan',
+            'pageSubTitle' => 'Tambah Data Hewan Kucing dan Anjing',
+        ]);
+    }
+
+    public function updateAnimal($id){
+
+        return view('internal.content.animal.animalUpdate', [
+            'title' => 'Data Hewan',
+            'pageTitle' => 'Ubah Data Hewan',
+            'pageSubTitle' => 'Ubah Data Hewan Kucing dan Anjing',
+            'id' => $id,
+        ]);
+    }
+
+    private function getListAnimal(){
+
+        $data = animal::all();
+
+        return response()->json($data);
+    }
+
 }
