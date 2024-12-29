c95fc96ca67e63b6047e976c709ff99109a1c368
Cheapfan <stevencokro32@gmail.com>
2024-12-29 16:18:14 +0700

    Hewan baru diselamatkan tidak dapat pindah ke detail

--- app/Http/Controllers/frontEnd/layanan/HewanDiselamatkanController.php
+++ app/Http/Controllers/frontEnd/layanan/HewanDiselamatkanController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\frontend\layanan;
 
 use App\Http\Controllers\Controller;
 use App\Models\animal;
-use App\Models\status;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
@@ -25,30 +24,4 @@ class HewanDiselamatkanController extends Controller
             'animals' => $animals
         ]);
     }
-
-    public function detail($animal_id)
-    {
-        $animal = animal::findOrFail($animal_id);
-
-        return view('frontend.pages.layanan.pengadopsian.detailHewan', [
-            'pagetitle' => 'Detail Hewan',
-            'animal' => $animal
-        ]);
-    }
-
-    // public function detail($animal_id){
-    //     $animal = animal::with(['status'])
-    //         ->findOrFail($animal_id);
-
-    //     if (!$animal) {
-    //         return redirect()->back()->with('error', 'Animal not found.');
-    //     }
-
-    //     $status = Status::where('config', 'Animal_Status')->get();
-
-    //     return view('frontend.pages.layanan.hewanDiselamatkan.detail', [
-    //         'pagetitle' => 'Hewan Diselaamatkan',
-    //         'animal' => $animal
-    //     ]);
-    // }
 }
