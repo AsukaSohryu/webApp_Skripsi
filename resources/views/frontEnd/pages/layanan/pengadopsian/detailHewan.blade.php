529af0551a17b5c5fefe85aa2bb024d1cab419cb
Samuel Michael <sam.michael26@gmail.com>
2025-01-09 11:39:20 +0700

    fixes to form layout

--- resources/views/frontEnd/pages/layanan/pengadopsian/detailHewan.blade.php
+++ resources/views/frontEnd/pages/layanan/pengadopsian/detailHewan.blade.php
@@ -35,59 +35,59 @@
             <div class="row my-1">
                 <div class="col">
                     <label for="" class="my-2">Nama Hewan</label>
-                    <input type="text" name="namaHewan" id="namaHewan" value="{{ $animal->animal_name }}" class="form-control text-muted" readonly>
+                    <input type="text" name="namaHewan" id="namaHewan" value="{{ $animal->animal_name }}" class="form-control text-muted" disabled>
                 </div>
             </div>
             <div class="row my-1">
                 <div class="col">
                     <label for="" class="my-2">Jenis Hewan</label>
-                    <input type="text" name="jenisHewan" id="jenisHewan" value="{{ $animal->animal_type }}" class="form-control text-muted" readonly>
+                    <input type="text" name="jenisHewan" id="jenisHewan" value="{{ $animal->animal_type }}" class="form-control text-muted" disabled>
                 </div>
                 <div class="col">
                     <label for="" class="my-2">Status Hewan</label>
-                    <input type="text" name="statusHewan" id="statusHewan" value="{{ $animal->status_name }}" class="form-control text-muted" readonly>
+                    <input type="text" name="statusHewan" id="statusHewan" value="{{ $animal->status_name }}" class="form-control text-muted" disabled>
                 </div>
             </div>
             <div class="row my-1">
                 <div class="col">
                     <label for="" class="my-2">Detail Status Hewan</label>
-                    <textarea name="detailStatusHewan" id="detailStatusHewan" rows="3" class="form-control text-muted" readonly>{{ $animal->detail_status }}</textarea>
+                    <textarea name="detailStatusHewan" id="detailStatusHewan" rows="3" class="form-control text-muted" disabled>{{ $animal->detail_status }}</textarea>
                 </div>
             </div>
             <div class="row my-1">
                 <div class="col">
                     <label for="" class="my-2">Usia Hewan</label>
-                    <input type="text" name="usiaHewan" id="usiaHewan" value="" class="form-control text-muted" readonly>
+                    <input type="text" name="usiaHewan" id="usiaHewan" value="" class="form-control text-muted" disabled>
                 </div>
                 <div class="col">
                     <label for="" class="my-2">Tanggal Lahir</label>
-                    <input type="text" name="tanggalLahir" id="tanggalLahir" value="{{ $animal->birth_date }}" class="form-control text-muted" readonly>
+                    <input type="text" name="tanggalLahir" id="tanggalLahir" value="{{ $animal->birth_date }}" class="form-control text-muted" disabled>
                 </div>
             </div>
             <div class="row my-1">
                 <div class="col">
                     <label for="" class="my-2">Jenis Kelamin</label>
-                    <input type="text" name="jenisKelamin" id="jenisKelamin" value="{{ $animal->sex }}" class="form-control text-muted" readonly>
+                    <input type="text" name="jenisKelamin" id="jenisKelamin" value="{{ $animal->sex }}" class="form-control text-muted" disabled>
                 </div>
                 <div class="col">
                     <label for="" class="my-2">Ras Hewan</label>
-                    <input type="text" name="rasHewan" id="rasHewan" value="{{ $animal->race }}" class="form-control text-muted" readonly>
+                    <input type="text" name="rasHewan" id="rasHewan" value="{{ $animal->race }}" class="form-control text-muted" disabled>
                 </div>
             </div>
             <div class="row my-1">
                 <div class="col">
                     <label for="" class="my-2">Warna Hewan</label>
-                    <input type="text" name="warnaHewan" id="warnaHewan" value="{{ $animal->color }}" class="form-control text-muted" readonly>
+                    <input type="text" name="warnaHewan" id="warnaHewan" value="{{ $animal->color }}" class="form-control text-muted" disabled>
                 </div>
                 <div class="col">
                     <label for="" class="my-2">Berat Hewan</label>
-                    <input type="text" name="beratHewan" id="beratHewan" value="{{ $animal->weight }}" class="form-control text-muted" readonly>
+                    <input type="text" name="beratHewan" id="beratHewan" value="{{ $animal->weight }}" class="form-control text-muted" disabled>
                 </div>
             </div>
             <div class="row my-1">
                 <div class="col">
                     <label for="" class="my-2">Informasi Vaksin Hewan</label>
-                    <input type="text" name="informasiVaksinHewan" id="informasiVaksinHewan" value="{{ $animal->vaccine }}" class="form-control text-muted" readonly>
+                    <input type="text" name="informasiVaksinHewan" id="informasiVaksinHewan" value="{{ $animal->vaccine }}" class="form-control text-muted" disabled>
                 </div>
             </div>
             <div class="row my-1">
@@ -96,43 +96,43 @@
                     <input type="text" name="apakahHewanSudahDisteril" id="apakahHewanSudahDisteril" 
                         value="@if($animal->is_sterile == 1) Sudah
                                 @else Belum @endif"  
-                        class="form-control text-muted" readonly>
+                        class="form-control text-muted" disabled>
                 </div>
             </div>
             <div class="row my-1">
                 <div class="col">
                     <label for="" class="my-2">Asal Hewan</label>
-                    <textarea name="asalHewan" id="asalHewan" rows="3" class="form-control text-muted" readonly>{{ $animal->source }}</textarea>
+                    <textarea name="asalHewan" id="asalHewan" rows="3" class="form-control text-muted" disabled>{{ $animal->source }}</textarea>
                 </div>
             </div>
             <div class="row my-1">
                 <div class="col">
                     <label for="" class="my-2">Karakteristik Hewan</label>
-                    <textarea name="karakteristikHewan" id="karakteristikHewan" rows="3" class="form-control text-muted" readonly>{{ $animal->characteristics }}</textarea>
+                    <textarea name="karakteristikHewan" id="karakteristikHewan" rows="3" class="form-control text-muted" disabled>{{ $animal->characteristics }}</textarea>
                 </div>
             </div>
             <div class="row my-1">
                 <div class="col">
                     <label for="" class="my-2">Deskripsi Hewan</label>
-                    <textarea name="deskripsiHewan" id="deskripsiHewan" rows="3" class="form-control text-muted" readonly>{{ $animal->description }}</textarea>
+                    <textarea name="deskripsiHewan" id="deskripsiHewan" rows="3" class="form-control text-muted" disabled>{{ $animal->description }}</textarea>
                 </div>
             </div>
             <div class="row my-1">
                 <div class="col">
                     <label for="" class="my-2">Catatan Medis Hewan</label>
-                    <textarea name="catatanMedisHewan" id="catatanMedisHewan" rows="3" class="form-control text-muted" readonly>{{ $animal->medical_note }}</textarea>
+                    <textarea name="catatanMedisHewan" id="catatanMedisHewan" rows="3" class="form-control text-muted" disabled>{{ $animal->medical_note }}</textarea>
                 </div>
             </div>
             <div class="row my-1">
                 <div class="col">
                     <label for="" class="my-2">Tanggal Hewan Masuk Shelter</label>
-                    <input type="text" name="tanggalHewanMasukShelter" id="tanggalHewanMasukShelter" value="{{ $animal->created_at }}" class="form-control text-muted" readonly>
+                    <input type="text" name="tanggalHewanMasukShelter" id="tanggalHewanMasukShelter" value="{{ $animal->created_at }}" class="form-control text-muted" disabled>
                 </div>
             </div>
             <div class="row my-1">
                 <div class="col">
                     <label for="" class="my-2">Tanggal Data Terakhir Diperbaharui</label>
-                    <input type="text" name="tanggalDataTerakhirDiperbaharui" id="tanggalDataTerakhirDiperbaharui" value="{{ $animal->updated_at }}" class="form-control text-muted" readonly>
+                    <input type="text" name="tanggalDataTerakhirDiperbaharui" id="tanggalDataTerakhirDiperbaharui" value="{{ $animal->updated_at }}" class="form-control text-muted" disabled>
                 </div>
             </div>
             <div class="row my-3">
